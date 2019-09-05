5cda4aea0e0d3f04ccc5c1c58865658839fc4859
sugam16 <54953550+sugam16@users.noreply.github.com>
2019-09-05 22:39:40 +0530

    Add files via upload

---
+++ src/main/java/com/example/openshift/SpringExampleApplication.java
@@ -0,0 +1,29 @@
+
+package com.example.openshift;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringExampleApplication {
+	
+	@GetMapping("/")
+	public String welcome()
+	{
+		return "Welcome to Java Techie";
+	}
+	
+	@GetMapping("/{input}")
+	public String congarts(@PathVariable String input)
+	{
+		return "Hi " + input + "your application deployed successfully";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
